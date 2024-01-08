9a9f64adafde26b4977c70e36fac81bd18f9b1c2
cucka <tsvetelina.milcheva@gmail.com>
2024-01-08 11:55:03 +0200

    third commit

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main(){
 
     cout<<"Hello GIT"<<endl;
-    cout<<"I am second commit line"<<endl;
+    cout<<"I am second commit line :)"<<endl;
+    cout<<"Third line"<<endl;
 
     return 0;
 }
