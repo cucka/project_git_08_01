7cc3e47ee303936d88c5dd27206074f44126e6cc
cucka <tsvetelina.milcheva@gmail.com>
2024-01-22 11:26:42 +0200

    new smile on 2nd line

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
 
     cout<<":) Hello GIT, hello from work, hello from home :)"<<endl;
-    cout<<"I am second commit line :)"<<endl;
+    cout<<":) I am second commit line :)"<<endl;
     cout<<"Third line :)"<<endl;
     cout<<"New line :)"<<endl;
 
