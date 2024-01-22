2324b97646e9f040ec8624ebbde70c4f8fc1a9de
cucka <tsvetelina.milcheva@gmail.com>
2024-01-22 11:02:40 +0200

    smile from home

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(){
 
     cout<<"Hello GIT, hello from work, hello from home"<<endl;
     cout<<"I am second commit line :)"<<endl;
-    cout<<"Third line"<<endl;
+    cout<<"Third line :)"<<endl;
 
     return 0;
 }
